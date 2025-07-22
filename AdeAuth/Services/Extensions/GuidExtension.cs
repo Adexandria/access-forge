4fa35a83ab95750474f9c58683797c37ef3ef572
Adeola Aderibigbe <adeola.aderibigbe@cyphercrescent.com>
2025-07-22 12:52:33 +0200

    Add extensions documentation

--- AdeAuth/Services/Extensions/GuidExtension.cs
+++ AdeAuth/Services/Extensions/GuidExtension.cs
@@ -6,8 +6,17 @@ using System.Threading.Tasks;
 
 namespace AdeAuth.Services.Extensions
 {
+    /// <summary>
+    /// Provides extension methods for the <see cref="Guid"/> structure.
+    /// </summary>
     internal static class GuidExtension
     {
+        /// <summary>
+        /// Determines whether the specified <see cref="Guid"/> is empty.
+        /// </summary>
+        /// <param name="guid">The <see cref="Guid"/> to check.</param>
+        /// <returns><see langword="true"/> if the <paramref name="guid"/> is equal to <see cref="Guid.Empty"/>; otherwise, <see
+        /// langword="false"/>.</returns>
         public static bool IsEmpty(this Guid guid)
         {
             return guid == Guid.Empty;
--- AdeAuth/Services/Extensions/StringExtension.cs
+++ AdeAuth/Services/Extensions/StringExtension.cs
@@ -6,8 +6,17 @@ using System.Threading.Tasks;
 
 namespace AdeAuth.Services.Extensions
 {
+    /// <summary>
+    /// Provides extension methods for string manipulation.
+    /// </summary>
     internal static class StringExtension
     {
+        /// <summary>
+        /// Determines if a string is empty or null.
+        /// </summary>
+        /// <param name="value">The value to check </param>
+        /// <returns><see langword="true"/> if the <paramref name="guid"/> is equal to <see cref="Guid.Empty"/>; otherwise, <see
+        /// langword="false"/>.</returns>
         public static bool IsEmpty(this string value)
         {
             return string.IsNullOrEmpty(value);
